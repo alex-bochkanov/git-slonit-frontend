382564ad075def4ccecf5aea42572ae3117f1818
alex-bochkanov <abochkanov94@yandex.ru>
2024-12-20 16:31:19 +0300

    Добавляет пример с кодом нейминга переменных

---
+++ naming-test.js
@@ -0,0 +1,21 @@
+// function pp(u) {
+//     let x = 0;
+//     u.forEach(i => {
+//       x += i;
+//     });
+//     return x;
+// }
+  
+// let aa = [10000, 20000, 30000];
+// console.log(pp(aa));
+
+function amountPurchases(purchasesArray) {
+    let amount = 0;
+    purchasesArray.forEach(purchase => {
+        amount += purchase;
+    });
+    return x;
+}
+
+let purchasesArray = [10000, 20000, 30000];
+console.log(amountPurchases(purchasesArray));
